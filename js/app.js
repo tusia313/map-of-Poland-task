313127d923b4bfaed57eb0eea5676415c2698fc9
Marta Łoskot <loskotmarta@gmail.com>
2024-01-16 20:21:37 +0100

    Finally have a map of Poland in svg format(code)

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+const mapOfPoland = document.getElementById('poland-map')
+
+function alertMe() {
+    alert('Kliknięto na mapę Polski!');
+}
+mapOfPoland.addEventListener('click', alertMe)
+  
+  
+  
